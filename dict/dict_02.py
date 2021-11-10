3af99a258b08dc3d7ed1e14ec9c6d3f40f6f76d6
Your Name <you@example.com>
2021-11-10 17:43:19 +0900

    third commit

--- dict/dict_02.py
+++ dict/dict_02.py
@@ -6,11 +6,12 @@ foods = {"떡볶이":"오뎅",
          "치킨":"치킨무",
          "삼겹살":"상추",
         } ;
-​
+
 while True:
-    myfood = input(str(list(foods.keys()))+"중 좋아하는 음식은?(끝내시려면 '끝'을 입력하세요.)")
+    myfood = input(f"{str(list(foods.keys()))} + 중 좋아하는 음식은?(끝내시려면 '끝'을 입력하세요.)")
     if myfood in foods :
-        print(f"{myfood} 궁합 음식은 {foods.get(myfood)}입니다.")
+        # print(f"{myfood} 궁합 음식은 {foods.get(myfood)}입니다.")
+        print(f"{myfood} 궁합 음식은 {foods[myfood]}입니다.")
     elif myfood == "끝":
         break
     else:
