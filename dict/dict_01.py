6b7b6530c01593324eaa894ac4f7168588761c3c
Your Name <you@example.com>
2021-11-10 17:18:23 +0900

    second commit

--- dict/dict_01.py
+++ dict/dict_01.py
@@ -5,6 +5,7 @@ singer['구성원 수'] = 9
 singer['데뷔'] = '서바이벌 식스틴'
 singer['대표곡'] = 'SIGNAL'
 
+# singer.keys() : dict_keys(['이름', '구성원 수', '데뷔', '대표곡'])
 for k in singer.keys() :
     print(f'{k} --> {singer[k]}')
 
---
+++ list/list_03.py
@@ -0,0 +1,8 @@
+
+# 1~5 정수 출력
+num_list = [num for num in range(1,6)]
+print(num_list)
+
+# 짝수만 출력
+num_even_list = [x for x in range(1,6) if x%2 == 0 ]
+print(num_even_list)
